fc5c029f1f93a5db30f2e453b50ff3c08dd7a422
PedroIvo <98492435+vegedra@users.noreply.github.com>
2024-06-10 20:04:07 -0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -7,97 +7,104 @@ int main() {
     int opc, qtde;  // Variavel que captura a escolha de menu do usuario
     float num1, num2, media, acumulador;
     
-    printf("*********** MENU CALCULADORA ***********");
-    printf("\n1) Adição");
-    printf("\n2) Subtração");
-    printf("\n3) Multiplicação");
-    printf("\n4) Divisão");
-    printf("\n5) Potência");
-    printf("\n6) Média");	
-    printf("\n7) Sair\n");
-    printf("\n****************************************\n");
+    opc = 0;
     
-    printf("\nDigite a opção desejada: ");
-    scanf("%9d", &opc);
+    do {
+        printf("*********** MENU CALCULADORA ***********");
+        printf("\n1) Adição");
+        printf("\n2) Subtração");
+        printf("\n3) Multiplicação");
+        printf("\n4) Divisão");
+        printf("\n5) Potência");
+        printf("\n6) Média");	
+        printf("\n7) Sair\n");
+        printf("\n****************************************\n");
     
-    /* Se deixar tudo if, independente da opcao ele carrega a primeira.
-    se usa então um if composto. 
+        printf("\nDigite a opção desejada: ");
+        scanf("%9d", &opc);
     
-    OPERADORES LÓGICOS: 
-    && = E 
-    || = OU
-    ! = NÃO/DIFERENTE
+        /* Se deixar tudo if, independente da opcao ele carrega a primeira.
+        se usa então um if composto. 
     
-    Para aparecer ou não as escolhas dos numeros caso o 
-    usuario nao queira sair.  */
-    if (opc >= 1 && opc <= 4){
-        printf("\nDigite o primeiro número: ");
-        scanf("%f", &num1);
+        OPERADORES LÓGICOS: 
+        && = E 
+        || = OU
+        ! = NÃO/DIFERENTE
     
-        printf("Digite o segundo número: ");
-        scanf("%f", &num2);
-    } 
-	
-	if (opc == 5) {
-        printf("\nDigite a base: ");
-        scanf("%f", &num1);
-    } 
-    
-    if (opc == 6) {
-        acumulador = 0;
-        num1 = 1;
-    
-        while (num1!= 0) {
-            printf("Digite o numero ou digite 0 para ver a soma: ");
+        Para aparecer ou não as escolhas dos numeros caso o 
+        usuario nao queira sair.  */
+        
+        if (opc >= 1 && opc <= 4){
+            printf("\nDigite o primeiro número: ");
             scanf("%f", &num1);
-            acumulador = num1+acumulador;
-            qtde++;
-        }
-        
-        if (num1 == 0) {
-            printf("Apresentando a soma...\n");
-            qtde--;
-            printf("O usuario digitou %d numeros e a soma é: %.2f\n", qtde--, acumulador);
-        
-            if (acumulador == 0 || qtde == 0) {
-                printf("Não é possível fazer essa média.");
-            } if (acumulador != 0 && qtde != 0) {
-                media = acumulador / qtde;
-                printf("A média de todos os números é: %.2f", media);
-            }
-        }
-        return 0;
-    }
     
-    // Caso o usuario digite algo maior que 5 ou menor que 0.  
-    if (opc > 8 || opc <= 0) {
-        printf("Opção Inválida.");
-        return 0;
-    }
-    
-    if (opc == 7) {
-        printf("Saindo...");
-        return 0;
-    } 
-    
-    // if encardeado: testa várias condições
-    if (opc == 1) {  // == é 'se for igual' 
-        printf("\n%.2f + %.2f = %.2f", num1, num2, num1+num2);
-    }
-    
-    if (opc == 2) {
-        printf("\n%.2f - %.2f = %.2f", num1, num2, num1-num2);
-    }
-    
-    if (opc == 3) {
-        printf("\n%.2f x %.2f = %.2f", num1, num2, num1*num2);
-    }
-    
-    if (opc == 4) {
-        printf("\n%.2f ÷ %.2f = %.2f", num1, num2, num1/num2);
-    }
+            printf("Digite o segundo número: ");
+            scanf("%f", &num2);
+        } 
 	
-	if (opc == 5) {
-        printf("\n%.2f ao quadrado = %.2f", num1, num1*num1);
-    }
+	    if (opc == 5) {
+            printf("\nDigite a base: ");
+            scanf("%f", &num1);
+        } 
+    
+        if (opc == 6) {
+            acumulador = 0;
+            num1 = 1;
+    
+            while (num1!= 0) {
+                printf("Digite o numero ou digite 0 para ver a soma: ");
+                scanf("%f", &num1);
+                acumulador = num1+acumulador;
+                qtde++;
+            }
+        
+            if (num1 == 0) {
+                printf("Apresentando a soma...\n");
+                qtde--;
+                printf("O usuario digitou %d numeros e a soma é: %.2f\n", qtde--, acumulador);
+        
+                if (acumulador == 0 || qtde == 0) {
+                    printf("Não é possível fazer essa média.");
+                } if (acumulador != 0 && qtde != 0) {
+                    media = acumulador / qtde;
+                    printf("A média de todos os números é: %.2f", media);
+                }
+            }
+            return 0;
+        }
+    
+        // Caso o usuario digite algo maior que 5 ou menor que 0.  
+        if (opc > 8 || opc <= 0) {
+            printf("Opção Inválida.");
+            return 0;
+        }
+    
+        if (opc == 7) {
+            printf("Saindo...");
+            return 0;
+        }    
+    
+        // if encardeado: testa várias condições
+        if (opc == 1) {  // == é 'se for igual' 
+            printf("\n%.2f + %.2f = %.2f", num1, num2, num1+num2);
+        }
+    
+        if (opc == 2) {
+            printf("\n%.2f - %.2f = %.2f", num1, num2, num1-num2);
+        }
+    
+        if (opc == 3) {
+            printf("\n%.2f x %.2f = %.2f", num1, num2, num1*num2);
+        }
+    
+        if (opc == 4) {
+            printf("\n%.2f ÷ %.2f = %.2f", num1, num2, num1/num2);
+        }
+	
+	    if (opc == 5) {
+            printf("\n%.2f ao quadrado = %.2f", num1, num1*num1);
+        }
+        
+    } while (opc != 0);
+    return 0;
 }
